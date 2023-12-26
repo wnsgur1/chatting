fee7fa6ad09f090ba75ba829769085ad77090be9
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2023-12-26 20:18:06 +0900

    📝:: DB Key
    DB에서 자주 사용하는 키를 companion object로 따로 분류

---
+++ app/src/main/java/kr/hs/dgsw/chatting/Key.kt
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.chatting
+
+class Key {
+    companion object{
+        const val DB_URL = "https://chatting-40314-default-rtdb.firebaseio.com/"
+        const val DB_USERS = "Users"
+        const val DB_CHAT_ROOMS = "ChatRooms"
+        const val DB_CHATS = "Chats"
+    }
+}
