932e2b10f8d7ca993293a5a2c5b438ce3e56dd43
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-01-06 17:10:21 +0900

    .

--- app/src/main/java/kr/hs/dgsw/chatting/LoginActivity.kt
+++ app/src/main/java/kr/hs/dgsw/chatting/LoginActivity.kt
@@ -7,7 +7,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kr.hs.dgsw.chatting.Key.Companion.DB_URL
+import kr.hs.dgsw.chatting.Key.Companion.DB_USERS
 import kr.hs.dgsw.chatting.databinding.ActivityLoginBinding
 
 class LoginActivity: AppCompatActivity() {
@@ -32,6 +35,8 @@ class LoginActivity: AppCompatActivity() {
                 .addOnCompleteListener(this){task ->
                     // 회원가입 성공
                     if(task.isSuccessful){
+
+
                         Log.d("TAG", "tjdrhd: ")
                         Toast.makeText(this, "회원가입에 성공했습니다. 로그인해주세요", Toast.LENGTH_SHORT).show()
 
@@ -56,8 +61,21 @@ class LoginActivity: AppCompatActivity() {
             }
             Firebase.auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) {taks ->
+
+                    val currentUser = Firebase.auth.currentUser
                     // 로그인 성공
-                    if(taks.isSuccessful){
+                    if(taks.isSuccessful && currentUser != null){
+                        val userId = currentUser.uid
+
+                        val user = mutableMapOf<String, Any>()
+                        // 데이터 클래스 변수이름과 일치시켜야함
+                        user["userId"] = userId
+                        user["userName"] = email
+
+                        Log.d("TAG", "onCreate: $userId, $email")
+
+                        // 파이어 베이스를 만든 위치가 미국이면 url를 생략할 수 있음
+                        Firebase.database(DB_URL).reference.child(DB_USERS).child(userId).updateChildren(user)
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
                         finish()
